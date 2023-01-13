acb4e654454e2363fbc509b111c53c2ba2e9aeaf
inksp0ts09 <92317108+inksp0ts09@users.noreply.github.com>
2023-01-12 22:00:05 -0500

    updated index.js

--- source/index.js
+++ source/index.js
@@ -50,5 +50,5 @@ function updateCity(event) {
 updateTime();
 setInterval(updateTime, 1000);
 
-let citiesSelectElement = document.querySelector("#city)");
+let citiesSelectElement = document.querySelector("#city");
 citiesSelectElement.addEventListener("change", updateCity);
